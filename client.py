29870632ca8cb71b3c3e4ea5e92d48329985f55d
Tair <abduraimovtair@gmail.com>
2020-12-23 15:14:28 +0200

    make class

--- client.py
+++ client.py
@@ -2,40 +2,66 @@ import socket
 import time
 import json
 
-HOST = '35.204.109.8'  # The server's hostname or IP address
+HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 3001        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    token = ""
-    for i in range(4):
-        send_data = ''
-        send_bytes = b''
-        if i == 0:
-            send_data = '{"data": "Hello, world"}'
-            send_bytes = send_data.encode()
-        if i == 1:
-            send_data = '{"auth": {"id": 8}}'
-            send_bytes = send_data.encode()
-        if i == 2:
-            send_data = '{"data": "ABVGD"}'
-            send_bytes = send_data.encode()
-        if i == 3:
-            send_data = '{"token": "' + token + '", "data": "ABVGD"}'
-            send_bytes = send_data.encode()
-        s.sendall(send_bytes)
-        data = s.recv(1024)
+class Hub:
+    def __init__(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        self.s = sock
+        self.on_listen = True
+            
+    
+    def send_data(self, data):
+        str_data = ""
+        if type(data) == str:
+            str_data = '"' + data + '"'
+        else:
+            str_data = str(data)
+        send_data = '{"data": ' + str_data
+        if self.token is not None:
+            send_data += ', "token": "' + self.token + '"}'
+        else:
+            send_data += '}'
+        self.send(send_data)
+
+    def send_auth(self, id):
+        str_data = str(id)
+        send_data = '{"auth": {"id": ' + str_data + '}}'
+        self.send(send_data)
+
+    def send(self, str_data):
+        send_bytes = str_data.encode()
+        self.s.sendall(send_bytes)
+        print('Sended', repr(str_data))
+        self.__listen()
+
+
+    def listen(self):
+        while self.on_listen:
+            self.__listen()
+            
+
+    def __listen(self):
+        data = self.s.recv(1024)
+        print('Received', repr(data))
         try:
             json_data = json.loads(data)
+            if 'data' in json_data.keys():
+                self.send_data("SAFFSASAF")
             if 'token' in json_data.keys():
-                token = json_data["token"]
+                self.token = json_data["token"]
         except NameError:
             print("Received not json")
-        print('Sended', repr(send_data))
-        print('Received', repr(data))
-        time.sleep(3)
+        
 
-    while 1:
-        data = s.recv(1024)
-        print('Received', repr(data))
+
+client = Hub()
+time.sleep(1)
+client.send_auth(8)
+time.sleep(3)
+client.send_data("SAFFSASAF")
+client.listen()
+
 
